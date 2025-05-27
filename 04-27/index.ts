6b4b51f8a59c11d4b65760c5e2d9ef361f1f38d6
Antonio Vasconcelos <antoniovs420@gmail.com>
2025-05-27 07:50:24 -0300

    04-27

--- 04-27/index.ts
+++ 04-27/index.ts
@@ -4,12 +4,14 @@ async function main() {
     const users = await getUsersUseCase();
     console.log(users);
 
-    
+
     /* 
         prox passos:
         criar um useCase que criar novos users
         criar um useCase que remove um user
         e realizar as chamadas na main
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+        
     */
 }
 
